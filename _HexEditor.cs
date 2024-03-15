f3308eaeafcd9c3c1230a906d149c5aba4fddf88
supchyan <mikhailsupov@yandex.ru>
2024-03-15 23:37:30 +0300

    ?

--- _HexEditor.cs
+++ _HexEditor.cs
@@ -29,56 +29,11 @@ namespace drak_mode_sai2 {
 
                 byte[] seeker = GetByteArray(_.Search);
                 byte[] hider = GetByteArray(_.Replace);
-                // from 2.10kk
-                // "f8f8f8" -> "000000" good
-                // "ffffff" -> "c12120" bad
-                // to 2.15kk
-
-                //0kk
-
-                //1kk
-                // from 500k to 700k nothing
-                // less 500k -> not working
-
-                // from 1.7kk
-                // "f8f8f8" -> "424242" good
-                // ffffff -> 424242 good
-                // c5c5c5 -> 212121 bad
-                // new drawing test -> crash
-                // to 4kk
-
-                // from 2.1kk
-                // "f8f8f8" -> "424242" good
-                // ffffff -> 424242 medium, not all fff recolored
-                // c5c5c5 -> 212121 bad
-                // "000000" -> "F5F5F5" crash
-                // new drawing test -> good
-                // to 2.3kk
-
-                // from 2260000
-                // "000000" -> "F5F5F5" bad
-                // to 2470410
+                
                 for (int i = from; i < to; i++) {
                     
                     if (!ContainsHEX(binaries, i, seeker)) continue;
                     
-                    // Console.WriteLine(i.ToString() + " " + new StringBuilder().Append(binaries.Skip(i).Take(23).Select(x => (char)x).ToArray()).ToString());
-                    // 2085392 = .data header begins
-                    // Probably pallete:
-                    
-                    // C0C0C0
-                    // FF3050 
-                    // D0D0D0
-                    // 204080
-                    // E0E0E0
-                    // F8F8F8
-                    // 90B0E8
-
-                    // 808080
-                    
-                    // 0800F9 FF?
-                    // E9FFE9 FF?
-                    // 0100E9 FF?
                     for (int j = from; j < to; j++) {
                         binaries[i + j] = hider[j];
                     }
