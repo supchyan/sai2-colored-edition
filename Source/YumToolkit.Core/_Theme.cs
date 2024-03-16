a9437c84f0214d78b9701b64ae43b88174c60f89
supchyan <mikhailsupov@yandex.ru>
2024-03-17 02:47:36 +0300

    .

--- Source/YumToolkit.Core/_Theme.cs
+++ Source/YumToolkit.Core/_Theme.cs
@@ -1,13 +1,13 @@
 namespace YumToolkit.Core {
     public class _Theme : _YumTools {
         public static void SetElementColor(byte[] color, int color_address) {
-            if(!File.Exists(_File.GetFileName.Dev)) {
+            if(!File.Exists(GetFileName.Dev)) {
                 Console.Clear();
                 _Console.WriteLine(ServiceMessage.DevFileIsNotExists, ConsoleColor.DarkRed);
                 return;
             }
 
-            byte[] binary = File.ReadAllBytes(_File.GetFileName.Dev);
+            byte[] binary = File.ReadAllBytes(GetFileName.Dev);
             
             // RGBA in SAI2 is BGRA. Live your life with that.
             (color[0], color[2]) = (color[2], color[0]);
@@ -15,7 +15,7 @@ namespace YumToolkit.Core {
             for(int i = 0; i < color.Length; i++) { binary[color_address + i] = color[i]; }
 
             try {
-                File.WriteAllBytes(_File.GetFileName.Classic, binary);
+                File.WriteAllBytes(GetFileName.Classic, binary);
                 Console.Clear();
                 _Console.Write($"Binary data overwritten!", ConsoleColor.Blue);
 
