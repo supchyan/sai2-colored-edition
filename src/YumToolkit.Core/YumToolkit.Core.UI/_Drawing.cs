50cb7b950fb9224cd19019327df3a2592a6b92e3
supchyan <mikhailsupov@yandex.ru>
2024-04-13 01:59:36 +0300

    compact

--- src/YumToolkit.Core/YumToolkit.Core.UI/_Drawing.cs
+++ src/YumToolkit.Core/YumToolkit.Core.UI/_Drawing.cs
@@ -1,4 +1,3 @@
-using System.IO;
 using YumToolkit.Global;
 
 namespace YumToolkit.Core.UI {
--- src/YumToolkit.Core/YumToolkit.Core.UI/_Watcher.cs
+++ src/YumToolkit.Core/YumToolkit.Core.UI/_Watcher.cs
@@ -3,27 +3,16 @@ using YumToolkit.Global;
 namespace YumToolkit.Core.UI {
     class _Watcher : _Globals {
         FileSystemWatcher tw = new FileSystemWatcher("./themes");
-
-        void FilesRenamed(object sender, RenamedEventArgs e) {
-            content.InvokeListUpdate();
-        }
-
-        void FilesCreated(object sender, FileSystemEventArgs e) {
-            content.InvokeListUpdate();
-        }
-
-        void FilesDeleted(object sender, FileSystemEventArgs e) {
-            content.InvokeListUpdate();
-        }
-
-        void FilesChanged(object sender, FileSystemEventArgs e) {
-            content.InvokeListUpdate();
-        }
+        void FilesRenamed(object sender, RenamedEventArgs e) { content.InvokeListUpdate(); }
+        void FilesCreated(object sender, FileSystemEventArgs e) { content.InvokeListUpdate(); }
+        void FilesDeleted(object sender, FileSystemEventArgs e) { content.InvokeListUpdate(); }
+        void FilesChanged(object sender, FileSystemEventArgs e) { content.InvokeListUpdate(); }
         public _Watcher() {
             tw.Changed += FilesChanged;
             tw.Created += FilesCreated;
             tw.Deleted += FilesDeleted;
             tw.Renamed += FilesRenamed;
+            
             tw.Filter = "*.json";
             tw.IncludeSubdirectories = true;
             tw.EnableRaisingEvents = true;
